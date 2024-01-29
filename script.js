2ab4a9d47368548cfab3a21d3c39f6e7cc0e381b
Stheven <sthevenalvess@gmail.com>
2024-01-29 12:15:43 -0300

    feat: database LocalStorage and screen rendering

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const createItem = (task, status='') => {
+let database = [
+    {'task': 'Estudar JS', 'status':''},
+    {'task': 'Ler', 'status': 'checked'}
+]
+
+const createItem = (task, status) => {
     const item = document.createElement('label');
     item.classList.add('todo_item');
     item.innerHTML = `
@@ -10,3 +15,17 @@ const createItem = (task, status='') => {
     `
     document.getElementById('todoList').appendChild(item);
 }
+
+const clearTask = () => {
+    const todoList = document.getElementById('todoList');
+    while(todoList.firstChild){
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const render = () => {
+    clearTask();
+    database.forEach(item => createItem(item.task, item.status));
+}
+
+render();
