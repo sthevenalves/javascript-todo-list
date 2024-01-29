1ac33f04f6fc85df72a91350cb48692ed61e1e6b
Stheven <sthevenalvess@gmail.com>
2024-01-29 12:04:02 -0300

    feat: function create item

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const createItem = (task, status='') => {
+    const item = document.createElement('label');
+    item.classList.add('todo_item');
+    item.innerHTML = `
+        <input type="checkbox" ${status}>
+        <div>${task}</div>
+        <input type="button" value="x">
+    `
+    document.getElementById('todoList').appendChild(item);
+}
