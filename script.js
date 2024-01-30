23aaa699a6114a2eb14f29f322b3aafcc6e875d5
Stheven <sthevenalvess@gmail.com>
2024-01-30 10:58:46 -0300

    feat:  send items to localstorage, getBase and setBase() functions

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 'use strict';
 
-let database = [
-    {'task': 'Estudar JS', 'status':''},
-    {'task': 'Ler', 'status': 'checked'}
-]
+// let database = [
+//     {'task': 'Estudar JS', 'status':''},
+//     {'task': 'Ler', 'status': 'checked'}
+// ]
+
+const getBase = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+
+const setBase = (database) => localStorage.setItem('todoList', JSON.stringify(database));
 
 const createItem = (task, status, indice) => {
     const item = document.createElement('label');
@@ -25,28 +29,43 @@ const clearTask = () => {
 
 const render = () => {
     clearTask();
+    const database = getBase();
     database.forEach((item, indice) => createItem(item.task, item.status, indice));
 }
 
 const insertItem = (event) => {
     const keyNow = event.key;
     if(keyNow === 'Enter'){
+        const database = getBase();
         database.push({'task': event.target.value, 'status': ''})
+        setBase(database);
         render();
         event.target.value = ''
     }
 }
 
 const removeItem = (indice) => {
+    const database = getBase();
     database.splice (indice, 1);
+    setBase(database);
     render();
 } 
+
+const updateItem = (indice) => {
+    const database = getBase();
+    database[indice].status = database[indice].status === '' ? 'checked' : '';
+    setBase(database);
+    render()
+}
 
 const clickItem = (event) => {
     const element = event.target;
     if(element.type === 'button'){
         const indice = element.dataset.indice;
         removeItem(indice);
+    }else if(element.type === 'checkbox'){
+        const indice = element.dataset.indice;
+        updateItem(indice);
     }
 
 }
